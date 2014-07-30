880ccde448f518e0657133402efaf4c2c9d6b24f
Hector Miranda <marcetux@hotmail.com>
2014-07-30 15:06:54 -0700

    Adding sketch that initializes and set a static IP allowin us to connect via telnet

---
+++ galileo/boardSetup/StartEthernet/StartEthernet.ino
@@ -0,0 +1,20 @@
+//Sketch to connect a PC host to Linux Galileo via telnet 
+// Written by M. Lastri fo garretlabs.wordpress.com
+//The sketch will:
+//1. set the Galileo IP to 169.254.1.1 
+//2. start telnet server
+//------>>>>>After this the user can:
+//3. From the pc host (i.e. with IP =169.254.1.2) send ping 169.254.1.1 and verify the answer
+//4. From the host send the famous command "telnet 169.254.1.1"...and the Linux Galileo shell should appear (user: root, no password)!
+//5. Enjoy!
+void setup() 
+{
+  system("ifconfig > /dev/ttyGS0");
+  system("ifconfig eth0 192.168.0.102 netmask 255.255.255.0 up");
+  system("ifconfig > /dev/ttyGS0");
+  system("telnetd -l /bin/sh");
+}
+void loop() 
+{
+  //nothing here 
+}
