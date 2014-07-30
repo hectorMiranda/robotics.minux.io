2c9b15074257cb8ba16c8289d340005ce0567caf
hectorMiranda <marcetux@hotmail.com>
2014-07-30 15:10:27 -0700

    Update StartEthernet.ino
    Update static IP address in comments

--- galileo/boardSetup/StartEthernet/StartEthernet.ino
+++ galileo/boardSetup/StartEthernet/StartEthernet.ino
@@ -1,11 +1,11 @@
 //Sketch to connect a PC host to Linux Galileo via telnet 
 // Written by M. Lastri fo garretlabs.wordpress.com
 //The sketch will:
-//1. set the Galileo IP to 169.254.1.1 
+//1. set the Galileo IP to 192.168.0.102
 //2. start telnet server
 //------>>>>>After this the user can:
 //3. From the pc host (i.e. with IP =169.254.1.2) send ping 169.254.1.1 and verify the answer
-//4. From the host send the famous command "telnet 169.254.1.1"...and the Linux Galileo shell should appear (user: root, no password)!
+//4. From the host send the famous command "telnet 192.168.0.102"...and the Linux Galileo shell should appear (user: root, no password)!
 //5. Enjoy!
 void setup() 
 {
